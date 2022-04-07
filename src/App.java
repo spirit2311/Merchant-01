e3a1faccd917b1917a92062cabb74346e2fc8a1d
spirit2311 <bogzabelin@gmail.com>
2022-04-07 20:45:06 +0300

    Change Main

--- src/App.java
+++ src/App.java
@@ -3,6 +3,6 @@ public class App {
 
     public static void main(String[] args) {
 
-        System.out.println("Привет! Это начало проекта.");
+        System.out.println("Привет! Это начало проекта. Разработка ведется в ветке develop");
     }
 }
